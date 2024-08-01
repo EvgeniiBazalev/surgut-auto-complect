3923e68de531f84d6f6889771981a5bd40b6fa31
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-01 08:42:44 +0500

    add CardStack and css for mainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CardStackMain from "@/components/pageComponents/mainPage/CardStackMain";
 import FlipWordsForMain from "@/components/pageComponents/mainPage/FlipWordsForMain";
 
 export default function Index() {
   return (
-    <>
+    <div className="mt-20 flex flex-col md:flex-row md:space-x-64 space-y-4 md:space-y-0">
       <FlipWordsForMain />
-    </>
+      <CardStackMain />
+    </div>
   );
 }
--- components/pageComponents/mainPage/FlipWordsForMain.tsx
+++ components/pageComponents/mainPage/FlipWordsForMain.tsx
@@ -4,7 +4,7 @@ const words = ["better", "cute", "beautiful", "modern"];
 
 export default function FlipWordsForMain() {
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
+    <div className="h-[15rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Build
         <FlipWords words={words} /> <br />
