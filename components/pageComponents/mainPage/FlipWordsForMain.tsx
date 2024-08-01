71362da29a0c243c2e0423a242d94d388d0f52a6
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-01 09:49:05 +0500

    text of flip words

--- components/pageComponents/mainPage/FlipWordsForMain.tsx
+++ components/pageComponents/mainPage/FlipWordsForMain.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { FlipWords } from "./FlipWords";
-const words = ["better", "cute", "beautiful", "modern"];
+
+const words = ["надежнее", "доступнее", "быстрее", "удобнее"];
 
 export default function FlipWordsForMain() {
   return (
     <div className="h-[15rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Build
-        <FlipWords words={words} /> <br />
-        websites with Aceternity UI
+        Покупайте автозапчасти
+        <FlipWords words={words} /> <br />в Сургуте с АвтоКомплект
       </div>
     </div>
   );
