d97ece203be1405d99d5567764cf74323935afae
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-01 10:15:05 +0500

    text mistakes fix

--- components/pageComponents/mainPage/FlipWordsForMain.tsx
+++ components/pageComponents/mainPage/FlipWordsForMain.tsx
@@ -8,7 +8,7 @@ export default function FlipWordsForMain() {
     <div className="h-[15rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         Покупайте автозапчасти
-        <FlipWords words={words} /> <br />в Сургуте с АвтоКомплект
+        <FlipWords words={words} /> <br />с СургутАвтоКомплект
       </div>
     </div>
   );
