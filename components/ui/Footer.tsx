66293fb7b9a93275b186eb5414e653d0706ae963
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-02 10:22:23 +0500

    footer

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
       <p>
-        Powered by{" "}
+        СургутАвтоКомплект {new Date().getFullYear()} <br />
         <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+          href="/"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Supabase
+          © Все права защищены
         </a>
       </p>
     </footer>
