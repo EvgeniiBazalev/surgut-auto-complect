8e0a51c203cf25d2fd4efff5dc744aded02392f3
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-06 17:34:27 +0500

    modal and loader

--- components/pageComponents/controlPage/AddCardsUI.tsx
+++ components/pageComponents/controlPage/AddCardsUI.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, Input } from "antd";
+import { Button, Input, Spin, Modal } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { uploadDataForMainCard } from "@/support/functions/uploadDataForMainCard";
 
@@ -13,11 +13,15 @@ export const AddCardsUI = ({ user }: { user: string }) => {
     url: "",
   });
 
+  const [loading, setLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const handleInputChange = (field: string, value: string) => {
     setCardInfo({ ...cardInfo, [field]: value });
   };
 
   const handleAddCard = async () => {
+    setLoading(true);
     try {
       await uploadDataForMainCard(cardInfo);
       setCardInfo({
@@ -26,40 +30,66 @@ export const AddCardsUI = ({ user }: { user: string }) => {
         content: "",
         url: "",
       });
+      setIsModalVisible(true);
     } catch (error) {
       console.error("Error adding card:", error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
   };
 
   return (
     <div>
-      <Input
-        placeholder="Name"
-        className="mt-4 w-full"
-        value={cardInfo.name}
-        onChange={(e) => handleInputChange("name", e.target.value)}
-      />
-      <Input
-        placeholder="Designation"
-        className="mt-4 w-full"
-        value={cardInfo.designation}
-        onChange={(e) => handleInputChange("designation", e.target.value)}
-      />
-      <TextArea
-        placeholder="Description"
-        className="mt-4 w-full"
-        value={cardInfo.content}
-        onChange={(e) => handleInputChange("content", e.target.value)}
-      />
-      <Input
-        placeholder="URL"
-        className="mt-4 w-full"
-        value={cardInfo.url}
-        onChange={(e) => handleInputChange("url", e.target.value)}
-      />
-      <Button type="default" className="mt-4" onClick={handleAddCard}>
-        Add Card
-      </Button>
+      <Spin spinning={loading}>
+        <Input
+          placeholder="Наименование промо акции"
+          className="mt-4 w-full"
+          value={cardInfo.name}
+          onChange={(e) => handleInputChange("name", e.target.value)}
+        />
+        <Input
+          placeholder="Обозначение"
+          className="mt-4 w-full"
+          value={cardInfo.designation}
+          onChange={(e) => handleInputChange("designation", e.target.value)}
+        />
+        <TextArea
+          placeholder="Описание"
+          className="mt-4 w-full"
+          value={cardInfo.content}
+          onChange={(e) => handleInputChange("content", e.target.value)}
+        />
+        <Input
+          placeholder="URL"
+          className="mt-4 w-full"
+          value={cardInfo.url}
+          onChange={(e) => handleInputChange("url", e.target.value)}
+        />
+        <Button
+          type="default"
+          className="mt-4"
+          onClick={handleAddCard}
+          disabled={loading}
+        >
+          Add Card
+        </Button>
+      </Spin>
+      <Modal
+        title="Успех"
+        open={isModalVisible}
+        onOk={handleCloseModal}
+        onCancel={handleCloseModal}
+        okButtonProps={{
+          className:
+            "bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded type-default",
+        }}
+      >
+        <p>Товар добавлен на сайт!</p>
+      </Modal>
     </div>
   );
 };
