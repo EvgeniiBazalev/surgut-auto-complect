8a97734aaea98e33f1f0b117d3dfde1bbfb8f25d
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-06 17:57:13 +0500

    file storage add

--- components/pageComponents/controlPage/AddCardsUI.tsx
+++ components/pageComponents/controlPage/AddCardsUI.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, Input, Spin, Modal } from "antd";
+import { Button, Input, Spin, Modal, Upload } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { uploadDataForMainCard } from "@/support/functions/uploadDataForMainCard";
+import { uploadImageToStorage } from "@/support/functions/uploadImageToStorage";
+import { UploadOutlined } from "@ant-design/icons";
 
 export const AddCardsUI = ({ user }: { user: string }) => {
   const [cardInfo, setCardInfo] = useState({
@@ -15,6 +17,7 @@ export const AddCardsUI = ({ user }: { user: string }) => {
 
   const [loading, setLoading] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
 
   const handleInputChange = (field: string, value: string) => {
     setCardInfo({ ...cardInfo, [field]: value });
@@ -23,19 +26,30 @@ export const AddCardsUI = ({ user }: { user: string }) => {
   const handleAddCard = async () => {
     setLoading(true);
     try {
-      await uploadDataForMainCard(cardInfo);
+      let imageUrl = "";
+      if (file) {
+        const imageUploadResponse = await uploadImageToStorage(file, user);
+        imageUrl = imageUploadResponse?.path || "";
+      }
+
+      await uploadDataForMainCard({ ...cardInfo, url: imageUrl });
       setCardInfo({
         name: "",
         designation: "",
         content: "",
         url: "",
       });
+      setFile(null); // сброс файла
       setIsModalVisible(true);
     } catch (error) {
       console.error("Error adding card:", error);
     } finally {
       setLoading(false);
     }
+  };
+
+  const handleFileChange = ({ file }: any) => {
+    setFile(file.originFileObj);
   };
 
   const handleCloseModal = () => {
@@ -46,49 +60,52 @@ export const AddCardsUI = ({ user }: { user: string }) => {
     <div>
       <Spin spinning={loading}>
         <Input
-          placeholder="Наименование промо акции"
+          placeholder="Название акции"
           className="mt-4 w-full"
           value={cardInfo.name}
           onChange={(e) => handleInputChange("name", e.target.value)}
         />
         <Input
-          placeholder="Обозначение"
+          placeholder="Промо акции"
           className="mt-4 w-full"
           value={cardInfo.designation}
           onChange={(e) => handleInputChange("designation", e.target.value)}
         />
         <TextArea
-          placeholder="Описание"
+          placeholder="Описание акции"
           className="mt-4 w-full"
           value={cardInfo.content}
           onChange={(e) => handleInputChange("content", e.target.value)}
         />
-        <Input
-          placeholder="Изображение"
+        <Upload
+          beforeUpload={() => false} // Отключить автоматическую загрузку
+          onChange={handleFileChange}
           className="mt-4 w-full"
-          value={cardInfo.url}
-          onChange={(e) => handleInputChange("url", e.target.value)}
-        />
+        >
+          <Button icon={<UploadOutlined />} className="mt-4">
+            Загрузить изображение
+          </Button>
+        </Upload>
         <Button
           type="default"
           className="mt-4"
           onClick={handleAddCard}
           disabled={loading}
         >
-          Добавить промо акцию
+          Добавить промо
         </Button>
       </Spin>
       <Modal
-        title="Ошибки отсутсвуют"
+        title="Успех"
         open={isModalVisible}
         onOk={handleCloseModal}
         onCancel={handleCloseModal}
         okButtonProps={{
           className:
-            "bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded type-default",
+            "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
         }}
       >
-        <p>Товар добавлен на сайт!</p>
+        <p>Промо акция добавлена на сайт!</p>
       </Modal>
     </div>
   );
---
+++ support/functions/uploadImageToStorage.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@/utils/supabase/client";
+
+export const uploadImageToStorage = async (file: File, userId: string) => {
+  const supabaseClient = createClient();
+  const filePath = `${userId}/${file.name}`;
+
+  const { data, error } = await supabaseClient.storage
+    .from("images")
+    .upload(filePath, file);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
