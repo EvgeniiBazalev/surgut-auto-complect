53da82ab57000ffebab4b9dd884e45147b9af9cb
EvgeniiBazalev <bazalevj@gmail.com>
2024-08-30 08:14:34 +0500

    strapi start

---
+++ app/strapi/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { fetchSparePartsStrapi } from "@/support/strapi/func";
+
+const page = () => {
+  const [spareParts, setSpareParts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadSpareParts = async () => {
+      setLoading(true);
+      const data = await fetchSparePartsStrapi();
+
+      if (data) {
+        console.log(data);
+        setSpareParts(data);
+      }
+
+      setLoading(false);
+    };
+
+    loadSpareParts();
+  }, []);
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  return <div>{<p>{spareParts}</p>}</div>;
+};
+
+export default page;
---
+++ support/strapi/func.tsx
@@ -0,0 +1,24 @@
+const apiUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+const token = process.env.STRAPI_API_TOKEN;
+
+export const fetchSparePartsStrapi = async () => {
+  try {
+    const response = await fetch(`${apiUrl}/api/spare-parts`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Ошибка при получении данных:", error);
+    return null;
+  }
+};
